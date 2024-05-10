ce8552fe5e2fd9f35c442039436875a91b94a61e
G vamshi <vamshigorige09@outlook.com>
2024-05-10 16:54:53 +0530

    commit

--- pat/pat1/admin.py
+++ pat/pat1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Batch,Company,Student
 # Register your models here.
+admin.site.register(Batch)
+admin.site.register(Company)
+admin.site.register(Student)
--- pat/pat1/models.py
+++ pat/pat1/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+class Batch(models.Model):
+    batch_year = models.CharField(primary_key=True,max_length=4)
+    
+    def __str__(self):
+        return (self.batch_year)
+class Student(models.Model):
+    batch_year=models.ForeignKey(Batch,on_delete=models.CASCADE)
+    student_name = models.CharField('Student name',max_length=30,null=True)
+    roll_num = models.CharField('Roll number',max_length=12,null=True)
+    email = models.EmailField('Student email',primary_key=True,max_length=40)
+    branch = models.CharField('Branch',null=True,max_length=20)
+
+    def __str__(self):
+        return (self.email)
+
+class Company(models.Model):
+    #batch_year = models.ForeignKey(Batch,on_delete=models.CASCADE)
+    students=models.ManyToManyField(Student)
+    company_name = models.CharField(primary_key=True,max_length=30)
+    first_round = models.BooleanField(null=False,default=False)
+    second_round = models.BooleanField(null = False,default=False)
+
+    def __str__(self):
+        return (self.company_name)
 # Create your models here.
